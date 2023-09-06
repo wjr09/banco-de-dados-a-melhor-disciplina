71ca8057395a3564253660e73fc99b20302f336b
Wellington Soares Ferreira Junior <100034210+wjr09@users.noreply.github.com>
2023-09-06 18:05:23 -0300

    Update listaExercicios01.sql
    exercício 18

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -88,3 +88,9 @@ LIMIT 1;
 SELECT produto, MIN(receita) AS menor_receita
 FROM vendas
 GROUP BY produto;
+
+-- exercício 18
+SELECT alunos.nome, COUNT(matriculas.id) AS numero_de_matriculas
+FROM alunos
+LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
+GROUP BY alunos.id;

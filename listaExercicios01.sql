e675603620c8806f290f9eb4c4a2b691f3a0874a
Wellington Soares Ferreira Junior <100034210+wjr09@users.noreply.github.com>
2023-09-06 17:56:41 -0300

    Update listaExercicios01.sql
    exercício 5

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -16,3 +16,8 @@ select alunos.nome
 from alunos 
 join matriculas on alunos.id = matriculas.aluno_id
 where matriculas.curso = 'Engenharia de Software';
+
+-- exercício 5
+SELECT produto, SUM(receita) as  receita_total
+FROM vendas
+GROUP BY produto;

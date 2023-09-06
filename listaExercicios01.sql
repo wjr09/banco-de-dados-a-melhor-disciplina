52f0d550a7ef8a6eb4c5daf9e03cb0a1ca1f56b1
Wellington Soares Ferreira Junior <100034210+wjr09@users.noreply.github.com>
2023-09-06 17:55:32 -0300

    Update listaExercicios01.sql
    exercício 3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,5 +1,12 @@
 -- exercicio 1
 SELECT titulo FROM livros;
+
 -- exercício 2
 select NOME from autores
 WHERE nascimento < '1900-01-01';
+
+-- exercício 3
+SELECT titulo 
+from livros 
+inner join autores on autor_id=autores.id
+where NOME = 'J.K. Rowling';

4759697968ce0c5ee648105e180b7654b754feb3
Wellington Soares Ferreira Junior <100034210+wjr09@users.noreply.github.com>
2023-09-06 18:02:58 -0300

    Update listaExercicios01.sql
    exercício 13

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -60,3 +60,8 @@ JOIN autores ON livros.autor_id = autores.id;
 SELECT alunos.nome, matriculas.curso
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id;
+
+-- exercício 13
+SELECT autores.nome, livros.titulo
+FROM autores
+LEFT JOIN livros ON autores.id = livros.autor_id;

03a36dc012aeeba4c8febe0afbb3a7fb6127b22d
Wellington Soares Ferreira Junior <100034210+wjr09@users.noreply.github.com>
2023-09-06 18:04:42 -0300

    Update listaExercicios01.sql
    exercício 17

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -83,3 +83,8 @@ LEFT JOIN livros ON autores.id = livros.autor_id
 GROUP BY autores.id
 ORDER BY numero_de_livros DESC
 LIMIT 1;
+
+-- exercício 17
+SELECT produto, MIN(receita) AS menor_receita
+FROM vendas
+GROUP BY produto;

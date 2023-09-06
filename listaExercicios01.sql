aa9b165338d5b6d5d3b5b03d0f831f82105f26af
Wellington Soares Ferreira Junior <100034210+wjr09@users.noreply.github.com>
2023-09-06 18:03:52 -0300

    Update listaExercicios01.sql
    exercício 15

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -70,3 +70,8 @@ LEFT JOIN livros ON autores.id = livros.autor_id;
 SELECT matriculas.curso, alunos.nome
 FROM matriculas
 RIGHT JOIN alunos ON matriculas.aluno_id = alunos.id;
+
+-- exercício 15 
+SELECT alunos.nome, matriculas.curso
+FROM alunos
+INNER JOIN matriculas ON alunos.id = matriculas.aluno_id;

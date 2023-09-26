d4be5be7786f28afb44eeadc828ab1523130ebf1
Wellington Soares Ferreira Junior <100034210+wjr09@users.noreply.github.com>
2023-09-26 08:54:37 -0300

    Update listaExercicios02.sql
    exercício 3

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -16,3 +16,12 @@ BEGIN
     WHERE Categoria.Nome = categoria_nome;
 END;
 //
+-- exercicio 3
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoria_nome VARCHAR(100), OUT total_livros INT)
+BEGIN
+    SELECT COUNT(*) INTO total_livros
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoria_nome;
+END;
+//

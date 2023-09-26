5577f8eb03cefe769dd1c0ccee6baa9482350c95
Wellington Soares Ferreira Junior <100034210+wjr09@users.noreply.github.com>
2023-09-26 08:53:27 -0300

    Update listaExercicios02.sql
    exercício 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -5,3 +5,14 @@ BEGIN
     SELECT * FROM Autor;
 END;
 //
+-- exercicio 2
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo, Autor.Nome, Autor.Sobrenome
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID
+    WHERE Categoria.Nome = categoria_nome;
+END;
+//

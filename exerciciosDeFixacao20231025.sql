e7bd591933cd5232d5db01f03efa612efe0a3e8b
Wellington Soares Ferreira Junior <100034210+wjr09@users.noreply.github.com>
2023-10-25 21:42:53 -0300

    Update exerciciosDeFixacao20231025.sql
    exercício 2

--- exerciciosDeFixacao20231025.sql
+++ exerciciosDeFixacao20231025.sql
@@ -4,3 +4,10 @@ CREATE TRIGGER insere_cliente
 AFTER INSERT ON clientes
 FOR EACH ROW
 INSERT INTO auditoria (mensagem) VALUES('novo cliente');
+
+-- exercicio 2
+
+CREATE TRIGGER excluir_cliente 
+BEFORE INSERT ON clientes
+FOR EACH ROW
+INSERT INTO auditoria (mensagem) VALUES('Tentativa de excluir');
